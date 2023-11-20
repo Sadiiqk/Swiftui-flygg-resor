65f77519f29fcb3ee69cc0596af43dc43ead534e
sadiiq kaysa <sadiiqk@gmail.com>
2023-11-20 15:33:05 +0100

    Initial Commit

---
+++ Swiftui-flygg-resor/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swiftui-flygg-resor
+//
+//  Created by don falcone on 2023-11-20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swiftui-flygg-resor/Swiftui_flygg_resorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_flygg_resorApp.swift
+//  Swiftui-flygg-resor
+//
+//  Created by don falcone on 2023-11-20.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_flygg_resorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
